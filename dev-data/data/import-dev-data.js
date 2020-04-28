089258a535d0f77b8854947d5a194d19669a5781
igorgo <igorgo@backbase.com>
2020-04-29 00:12:22 +0200

    import-dev-data script was updated

--- dev-data/data/import-dev-data.js
+++ dev-data/data/import-dev-data.js
@@ -2,6 +2,8 @@ const fs = require('fs');
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 const Tour = require('../../models/tour.model');
+const User = require('../../models/user.model');
+const Review = require('../../models/review.model');
 
 dotenv.config({ path: '../../config.env' });
 
@@ -14,12 +16,18 @@ mongoose
   })
   .then(() => console.log('DB connection successful'));
 
-const toursRaw = fs.readFileSync(`${__dirname}/tours.json`, 'utf-8');
-const tours = JSON.parse(toursRaw);
+const tours = JSON.parse(fs.readFileSync(`${__dirname}/tours.json`, 'utf-8'));
+const users = JSON.parse(fs.readFileSync(`${__dirname}/users.json`, 'utf-8'));
+const reviews = JSON.parse(
+  fs.readFileSync(`${__dirname}/reviews.json`, 'utf-8')
+);
 
 const importData = async () => {
   try {
     await Tour.create(tours);
+    // to remove from user model password encryption, because it's already encrypted in test data
+    await User.create(users, { validateBeforeSave: false });
+    await Review.create(reviews);
     console.log('Data successfully loaded');
   } catch (e) {
     console.log(e);
@@ -31,6 +39,8 @@ const importData = async () => {
 const deleteData = async () => {
   try {
     await Tour.deleteMany();
+    await User.deleteMany();
+    await Review.deleteMany();
     console.log('Data successfully deleted');
   } catch (e) {
     console.log(e);
